867ab537b29db4e28ad90a9848100b9bd4988015
ErnestF22 <ernesto.fontana@unipr.it>
2026-02-10 18:21:12 +0100

    Added output gathering and saving to file; added cfg file

--- scan_overlap/app/run_graph_tests.cpp
+++ scan_overlap/app/run_graph_tests.cpp
@@ -22,6 +22,9 @@
 
 #include "transform_utils.h"
 
+#include <rofl/common/profiler.h>
+#include <rofl/common/io.h>
+
 class GraphSolver
 {
 public:
@@ -91,6 +94,7 @@ int main(int argc, char **argv)
 {
     // reading params and graph
     std::string filenameCfg, filenameGraph;
+    bool enableArs, enableIcp, enableVfc, enableArsGraph;
     rofl::ParamMap params;
 
     // Reads params from command line
@@ -98,7 +102,11 @@ int main(int argc, char **argv)
     params.getParam<std::string>("cfg", filenameCfg, std::string(""));
     params.read(filenameCfg);
     params.read(argc, argv);
-    params.getParam<std::string>("graph", filenameGraph, std::string(""));
+    params.getParam<std::string>("in", filenameGraph, std::string(""));
+    params.getParam<bool>("enable_ars", enableArs, false);
+    params.getParam<bool>("enable_icp", enableIcp, false);
+    params.getParam<bool>("enable_vfc", enableVfc, false);
+    params.getParam<bool>("enable_ars_graph", enableArsGraph, false);
 
     std::cout << "\nParams:" << std::endl;
     params.write(std::cout);
@@ -115,157 +123,153 @@ int main(int argc, char **argv)
      * ARS
      */
     // compute coeffs, used for both ars pairwise and ars graph
-    for (auto &n : nodes)
-    {
-        ars::AngularRadonSpectrum2d arsSrc;
 
-        int fourierOrder = 30; // TODO: make ARS params configurable from file/command line
-        double sigma = 0.05;
-
-        arsSrc.setARSFOrder(fourierOrder);
-
-        arsSrc.initLUT(0.0001);
-        arsSrc.setComputeMode(ars::ArsKernelIsotropic2d::ComputeMode::PNEBI_LUT);
-
-        auto timeStart = std::chrono::system_clock::now();
-        ars::VectorVector2 acesPoints1;
-        for (auto &p : n.cloud)
-            acesPoints1.push_back(ars::Vector2(p.x(), p.y()));
-
-        arsSrc.insertIsotropicGaussians(acesPoints1, sigma);
-
-        std::cout << "ars.coefficients().at(0) " << arsSrc.coefficients().at(0) << ", ars.coefficients().at(2) " << arsSrc.coefficients().at(2) << std::endl;
-
-        auto timeStop = std::chrono::system_clock::now();
-        double timeAvg = (double)std::chrono::duration_cast<std::chrono::milliseconds>(timeStop - timeStart).count();
-        std::cout << "insertIsotropicGaussians() " << timeAvg << " ms" << std::endl;
-
-        std::cout << "\n------\n"
-                  << std::endl;
-
-        std::cout << "\nARS Coefficients:\n";
-        std::cout << "\ti \tDownward \tLUT\n";
-        for (int i = 0; i < arsSrc.coefficients().size(); ++i)
+    if (enableArs)
+        for (auto &n : nodes)
         {
-            std::cout << "\t" << i << " \t" << arsSrc.coefficients().at(i) << "\n";
+            std::cout << "Node " << n.id << ", cloud size " << n.cloud.size() << std::endl;
+
+            ars::AngularRadonSpectrum2d arsSrc;
+
+            int fourierOrder = 30; // TODO: make ARS params configurable from file/command line
+            double sigma = 0.05;
+
+            arsSrc.setARSFOrder(fourierOrder);
+
+            arsSrc.initLUT(0.0001);
+            arsSrc.setComputeMode(ars::ArsKernelIsotropic2d::ComputeMode::PNEBI_LUT);
+
+            auto timeStart = std::chrono::system_clock::now();
+            ars::VectorVector2 acesPoints1;
+            for (auto &p : n.cloud)
+                acesPoints1.push_back(ars::Vector2(p.x(), p.y()));
+
+            arsSrc.insertIsotropicGaussians(acesPoints1, sigma);
+
+            std::cout << "ars.coefficients().at(0) " << arsSrc.coefficients().at(0) << ", ars.coefficients().at(2) " << arsSrc.coefficients().at(2) << std::endl;
+
+            auto timeStop = std::chrono::system_clock::now();
+            double timeAvg = (double)std::chrono::duration_cast<std::chrono::milliseconds>(timeStop - timeStart).count();
+            std::cout << "insertIsotropicGaussians() " << timeAvg << " ms" << std::endl;
+
+            std::cout << "\n------\n"
+                      << std::endl;
+
+            std::cout << "\nARS Coefficients:\n";
+            std::cout << "\ti \tDownward \tLUT\n";
+            for (int i = 0; i < arsSrc.coefficients().size(); ++i)
+            {
+                std::cout << "\t" << i << " \t" << arsSrc.coefficients().at(i) << "\n";
+            }
+            std::cout << std::endl;
+
+            n.setCoeffs(arsSrc.coefficients());
         }
-        std::cout << std::endl;
 
-        n.setCoeffs(arsSrc.coefficients());
-    }
+    // scan_overlap::Node src = nodes.front();
+    // scan_overlap::Node dst = nodes.back();
 
-    scan_overlap::Node src = nodes.front();
-    scan_overlap::Node dst = nodes.back();
+    // std::vector<double> funcFourierRecursDownLUT;
+    // std::vector<double> funcFourierRecursDown;
+    // int thnum = 360;
+    // double dtheta = M_PI / thnum;
+    // double theta;
+    // for (int i = 0; i < thnum; ++i)
+    // {
+    //     theta = dtheta * i;
+    //     funcFourierRecursDownLUT.push_back(ars::evaluateFourier(src.coeffs, theta));
+    //     funcFourierRecursDown.push_back(ars::evaluateFourier(dst.coeffs, theta));
+    // }
 
-    std::vector<double> funcFourierRecursDownLUT;
-    std::vector<double> funcFourierRecursDown;
-    int thnum = 360;
-    double dtheta = M_PI / thnum;
-    double theta;
-    for (int i = 0; i < thnum; ++i)
-    {
-        theta = dtheta * i;
-        funcFourierRecursDownLUT.push_back(ars::evaluateFourier(src.coeffs, theta));
-        funcFourierRecursDown.push_back(ars::evaluateFourier(dst.coeffs, theta));
-    }
+    // std::vector<double> correlationFourier;
+    // ars::computeFourierCorr(src.coeffs, dst.coeffs, correlationFourier);
 
-    std::vector<double> correlationFourier;
-    ars::computeFourierCorr(src.coeffs, dst.coeffs, correlationFourier);
-
-    double arsThetaToll = 1.0f, fourierTol = 1.0f;
-    double thetaMax, corrMax;
-    ars::findGlobalMaxBBFourier(correlationFourier, 0.0, M_PI, arsThetaToll, fourierTol, thetaMax, corrMax);
-    double rotArs = thetaMax;
+    // double arsThetaToll = 1.0f, fourierTol = 1.0f;
+    // double thetaMax, corrMax;
+    // ars::findGlobalMaxBBFourier(correlationFourier, 0.0, M_PI, arsThetaToll, fourierTol, thetaMax, corrMax);
+    // double rotArs = thetaMax;
 
     /**
      * ICP
      */
 
-    // define a 3 dim problem with 10000 model points
-    // and 10000 template points:
-    int32_t dim = 3;
-    int32_t num = 10000;
-
-    // allocate model and template memory
-    double *M = (double *)calloc(3 * num, sizeof(double));
-    double *T = (double *)calloc(3 * num, sizeof(double));
-
-    // set model and template points
-    std::cout << endl
-              << "Creating model with 10000 points ..." << endl;
-    cout << "Creating template by shifting model by (1,1,1) ..." << endl;
-    int32_t k = 0;
-    for (double x = -2; x < 2; x += 0.04)
+    std::vector<double> anglesIcp;
+    if (enableIcp)
     {
-        for (double y = -2; y < 2; y += 0.04)
-        {
-            double z = 5 * x * exp(-x * x - y * y);
-            M[k * 3 + 0] = x;
-            M[k * 3 + 1] = y;
-            M[k * 3 + 2] = z;
-            T[k * 3 + 0] = x - 1;
-            T[k * 3 + 1] = y - 1;
-            T[k * 3 + 2] = z - 1;
-            k++;
-        }
+        GraphSolverIcp solverIcp;
+        solverIcp.estimate(nodes, odoms, edges, anglesIcp);
     }
-
-    // start with identity as initial transformation
-    // in practice you might want to use some kind of prediction here
-    Matrix R = Matrix::eye(3);
-    Matrix t(3, 1);
-
-    // run point-to-plane ICP (-1 = no outlier threshold)
-    cout << endl
-         << "Running ICP (point-to-plane, no outliers)" << endl;
-    IcpPointToPlane icp(M, num, dim);
-    icp.fit(T, num, R, t, -1);
-
-    // results
-    cout << endl
-         << "Transformation results:" << endl;
-    cout << "R:" << endl
-         << R << endl
-         << endl;
-    cout << "t:" << endl
-         << t << endl
-         << endl;
-
-    // free memory
-    free(M);
-    free(T);
 
     /**
      * VFC
      */
 
-    cv::Mat img_1, img_2;
-    int vfcMethod = 1; // 0: SURF; 1: ORB
-
-    if (!img_1.data || !img_2.data || img_1.rows != img_2.rows || img_1.cols != img_2.cols)
+    std::vector<double> anglesVfc;
+    if (enableVfc)
     {
-        return -1;
+        GraphSolverVfc solverVfc;
+        solverVfc.estimate(nodes, odoms, edges, anglesVfc);
     }
 
-    std::vector<KeyPoint> keypoints_1, keypoints_2;
-    Mat descriptors_1, descriptors_2;
-    std::vector<DMatch> matches, correctMatches;
+    /**
+     * ARS Graph
+     */
+    std::vector<double> anglesArsGraph;
+    if (enableArsGraph)
+    {
+        GraphSolverArsGraph solverArsGraph;
+        solverArsGraph.estimate(nodes, odoms, edges, anglesArsGraph);
+    }
 
-    // initial matching by SURF or ORB
-    // if (vfcMethod == 0) {
-    //     surfInitMatchImagePair(img_1, img_2, keypoints_1, keypoints_2,
-    //             descriptors_1, descriptors_2, matches);
-    // } else {
-    //     orbInitMatchImagePair(img_1, img_2, keypoints_1, keypoints_2,
-    //             descriptors_1, descriptors_2, matches);
-    // }
+    /**
+     * ARS
+     */
+    std::vector<double> anglesArs;
+    if (enableArs)
+    {
+        GraphSolverArs solverArs;
+        solverArs.estimate(nodes, odoms, edges, anglesArs);
+    }
 
-    // //  Remove mismatches by vector field consensus (VFC)
-    // vfcMatch(keypoints_1, keypoints_2, matches, correctMatches);
-
-    // // visualization
-    // visualizeMatchingResults(img_1, img_2, keypoints_1, keypoints_2,
-    //         matches, correctMatches);
+    /**
+     * Gathering results
+     */
+    std::string methodsEnabled = "";
+    if (enableIcp)
+        methodsEnabled += "icp_";
+    if (enableVfc)
+        methodsEnabled += "vfc_";
+    if (enableArs)
+        methodsEnabled += "arspw_";
+    if (enableArsGraph)
+        methodsEnabled += "arsgraph_";
+    ROFL_VAR1(methodsEnabled);
+    std::string filenameOut = rofl::generateStampedString("results_" + methodsEnabled, ".csv");
+    ROFL_VAR1(filenameOut);
+    std::ofstream fileOut(filenameOut);
+    fileOut << "id,gt,odom,";
+    if (enableIcp)
+        fileOut << "icp,";
+    if (enableVfc)
+        fileOut << "vfc,";
+    if (enableArs)
+        fileOut << "ars,";
+    if (enableArsGraph)
+        fileOut << "ars_graph";
+    fileOut << "\n";
+    for (int i = 0; i < nodes.size(); ++i)
+    {
+        fileOut << nodes.at(i).id << "," << atan2(gts.at(i).linear().col(0).y(), gts.at(i).linear().col(0).x()) << "," << atan2(odoms.at(i).linear().col(0).y(), odoms.at(i).linear().col(0).x()) << ",";
+        if (enableIcp)
+            fileOut << anglesIcp.at(i) << ",";
+        if (enableVfc)
+            fileOut << anglesVfc.at(i) << ",";
+        if (enableArs)
+            fileOut << anglesArs.at(i) << ",";
+        if (enableArsGraph)
+            fileOut << anglesArsGraph.at(i);
+        fileOut << "\n";
+    }
 
     return 0;
 }
@@ -350,23 +354,25 @@ void GraphSolverArs::estimate(const std::vector<scan_overlap::Node> &nodes,
 }
 
 void GraphSolverArsGraph::estimate(const std::vector<scan_overlap::Node> &nodes,
-                              const scan_overlap::VectorTransform2 &odoms,
-                              const std::vector<scan_overlap::Edge> &edges,
-                              std::vector<double> &angles)
+                                   const scan_overlap::VectorTransform2 &odoms,
+                                   const std::vector<scan_overlap::Edge> &edges,
+                                   std::vector<double> &angles)
 {
     ars::ArsGraph::Ptr graph(new ars::ArsGraph);
-    ars::ArsGraphIntervalFull::Ptr interval(new ars::ArsGraphIntervalFull); 
+    ars::ArsGraphIntervalFull::Ptr interval(new ars::ArsGraphIntervalFull);
     graph->setFourierOrder(30);
 
     std::map<int, int> idToArsId;
 
-    for (int i = 1; i < nodes.size(); ++i){
+    for (int i = 1; i < nodes.size(); ++i)
+    {
         const auto &n = nodes.at(i);
         graph->addNode(n.coeffs);
         idToArsId[n.id] = i;
     }
 
-    for(const auto& edge : edges){
+    for (const auto &edge : edges)
+    {
         int src = idToArsId[edge.src];
         int dst = idToArsId[edge.dst];
         graph->addEdgeWithDerivative(src, dst);
